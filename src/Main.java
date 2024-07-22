d2e6d78e96a986ce4f524f525f2d3cf3220f5530
latentkiller47 <1132285347@qq.com>
2024-07-22 22:13:34 +0800

    master commit

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.printf("Hello and welcome!");
         System.out.printf("Hello and welcome!");
         System.out.printf("Hello and welcome!");
+        System.out.printf("master test!");
 
         for (int i = 1; i <= 5; i++) {
             //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
